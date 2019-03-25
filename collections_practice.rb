9b97cf1e0e9525ac04ecb9d01a4795ad79082a42
Michael Mottola <mottola64@gmail.com>
2019-03-25 00:35:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,5 +22,12 @@ def reverse_array(array)
   return reversed
 end
 
-def kesha_maker
+def kesha_maker(array)
   kesha = []
+  array.each do |word|
+    word_array = word.split
+    word_array[2] = "$"
+    kesha << word_array.join
+  end
+  kesha
+end
